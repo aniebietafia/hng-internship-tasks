3dd386f8fec04c95bf12b777c2407c8c268dbd04
aniebietafia <aniebietafia87@gmail.com>
2023-09-07 20:54:42 +0100

    task controller

--- stage_one_task/src/controllers/stage_one_task.controller.js
+++ stage_one_task/src/controllers/stage_one_task.controller.js
@@ -24,29 +24,29 @@ export const getStageOneTask = async (req, res) => {
     const days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     const current_day_of_the_week = date.getDay();
-    const day_of_the_week = days_of_the_week[current_day_of_the_week];
+    const current_day = days_of_the_week[current_day_of_the_week];
 
     // current UTC time, accurate within a +/-2 minute window.
     // const current_utc_time = JSON.stringify(date);
-    const current_utc_time = date;
+    const utc_time = date;
 
     // github url of the file being run
-    const github_url_of_the_file_being_run =
+    const github_file_url =
       "https://github.com/aniebietafia/hng-internship-tasks/blob/main/stage_one_task/src/server.js";
 
     // github url of the full source code
-    const github_url_of_the_full_source_code = "https://github.com/aniebietafia/hng-internship-tasks";
+    const github_repo_url = "https://github.com/aniebietafia/hng-internship-tasks";
 
     // status code of success
     const status_code = 200;
 
     const data = {
       slack_name,
-      day_of_the_week,
-      current_utc_time,
+      current_day,
+      utc_time,
       track,
-      github_url_of_the_file_being_run,
-      github_url_of_the_full_source_code,
+      github_file_url,
+      github_repo_url,
       status_code,
     };
 
