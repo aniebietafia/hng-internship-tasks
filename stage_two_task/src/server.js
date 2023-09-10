17e3ae1f83ff30910197e467f7599f5a92442de0
aniebietafia <aniebietafia87@gmail.com>
2023-09-10 22:35:47 +0100

    server setup

--- stage_two_task/src/server.js
+++ stage_two_task/src/server.js
@@ -3,7 +3,26 @@ import http from "http";
 import "dotenv/config";
 
 // import app
-import app from "./app";
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+
+const PORT = process.env.PORT || 5000;
 
 // create server
 const server = http.createServer(app);
+
+const startServer = async () => {
+  try {
+    // connect to database
+    await connectDB();
+
+    // start server
+    server.listen(PORT, () => {
+      console.log(`🚀 Server is running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Oops! Something went wrong: ${error.message}`);
+    process.exit(1);
+  }
+};
+startServer();
