e08911f8681d92a7ce380d428988a218875a8933
aniebietafia <aniebietafia87@gmail.com>
2023-09-10 22:35:14 +0100

    setting up database connection

--- stage_two_task/src/config/database.js
+++ stage_two_task/src/config/database.js
@@ -2,13 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
-
-    console.log(`Database Connected Successfully: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Database Connected Successfully.`);
   } catch (error) {
     console.error(`Oops! Connection failed. Try again.: ${error.message}`);
     process.exit(1);
