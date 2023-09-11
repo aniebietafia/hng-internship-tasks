ea4f6aebc29e07b196466659f764e35eea05d654
aniebietafia <aniebietafia87@gmail.com>
2023-09-11 15:28:35 +0100

    initializing app

--- stage_two_task/src/app.js
+++ stage_two_task/src/app.js
@@ -4,12 +4,14 @@ import express from "express";
 const app = express(); // initialize express
 
 // import routes
+import personRoutes from "./routes/person.route.js";
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // mount routes
+app.use(personRoutes);
 
 // export app
 export default app;
