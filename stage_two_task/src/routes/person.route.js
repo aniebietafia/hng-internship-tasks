2f633241fe1bbe5b1e882eec17a14e126bfac6ce
aniebietafia <aniebietafia87@gmail.com>
2023-09-11 17:54:38 +0100

    adding delete route

--- stage_two_task/src/routes/person.route.js
+++ stage_two_task/src/routes/person.route.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 
 // import controller
-import { fetchPerson, createPerson } from "../controllers/person.controller.js";
+import { fetchPerson, createPerson, updatePerson, deletePerson } from "../controllers/person.controller.js";
 
 // initialize router
 const router = Router();
 
 // define routes
 router.post("/api", createPerson);
-router.get("/api/user_id", fetchPerson);
-router.put("/api/:user_id");
-router.delete("/api/:user_id");
+router.get("/api/:user_id", fetchPerson);
+router.put("/api/:user_id", updatePerson);
+router.delete("/api/:user_id", deletePerson);
 
 // export default router
 export default router;
